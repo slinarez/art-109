9b8194fb422bab3e3ef682f4538acbc356b1b978
Sophia L <slinarez423@gmail.com>
2025-11-07 23:37:04 -0800

    three.js

--- three-00-starter/js/main.js
+++ three-00-starter/js/main.js
@@ -1,7 +1,6 @@
 // Basic Three.JS scene from documentation, importing Three.JS through a CDN 
 // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
 
-
 //~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
 import * as THREE from 'three';
 
@@ -10,24 +9,69 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
-// ~~~~~~~~~~~~~~~~Set up~~~~~~~~~~~~~~~~
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let scene, camera, renderer, cube;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function init() {
+
+    // ~~~~~~~~~~~~~~~~Set up~~~~~~~~~~~~~~~~
+    scene = new THREE.Scene();
+
+    const light = new THREE.DirectionalLight(0xffffff, 8);
+    light.position.set(1,1,5);
+    scene.add(light);
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
 
 
-// ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
-const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader(); // to load 3d models
+    // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const loader = new GLTFLoader(); // to load 3d models
+
+    loader.load('assets/dog_shiny.gltf', function (gltf) {
+        const dog = gltf.scene;
+        scene.add(dog);
+        dog.scale.set(3,3,3);
+    })
 
 
+    // ~~~~~~~~~~~~~~~~ Create scene here ~~~~~~~~~~~~~~~~
+    // →→→→→→ Follow next steps in tutorial: 
+    // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
 
-// ~~~~~~~~~~~~~~~~ Create scene here ~~~~~~~~~~~~~~~~
-// →→→→→→ Follow next steps in tutorial: 
-// // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+    const geometry = new THREE.CapsuleGeometry(1, 1, 3, 4);
+    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const texture = new THREE.TextureLoader().load('textures/lavatile.jpg')
+    const material = new THREE.MeshStandardMaterial({ map: texture})
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 7;
+
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
 
 
